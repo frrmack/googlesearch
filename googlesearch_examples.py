0fc078f1d12d0de23a37425aa1992039aaa937f4
Irmak Sirer <irmak.sirer@datascopeanalytics.com>
2015-05-25 18:53:07 -0500

    fixed indentation issue in the simplified examples

--- googlesearch_examples.py
+++ googlesearch_examples.py
@@ -23,17 +23,17 @@ def search_wikipedia(query):
 def x_vs_y_count_match(x, y):
     """Which of the two words is used more 
     on the Internet?"""
-	nx = GoogleSearch(x).count()
-	ny = GoogleSearch(y).count()
-	print '%s vs %s:' % (x,y)
-	report = '%s wins with %i vs %i'
-	if   nx > ny:
-	    print report % (x,nx,ny)
-	elif nx < ny:
-	    print report % (y,ny,nx)
-	else:
-	    print "it's a tie with %s each!" % nx
-	return nx, ny
+    nx = GoogleSearch(x).count()
+    ny = GoogleSearch(y).count()
+    print '%s vs %s:' % (x,y)
+    report = '%s wins with %i vs %i'
+    if   nx > ny:
+        print report % (x,nx,ny)
+    elif nx < ny:
+        print report % (y,ny,nx)
+    else:
+        print "it's a tie with %s each!" % nx
+    return nx, ny
 
 
 def imdb_id_for_movie(movie_name):
