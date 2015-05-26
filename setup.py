67d44652bc01c9091f3e4cba5658ae8d5c649538
Irmak Sirer <irmak.sirer@datascopeanalytics.com>
2015-05-25 20:45:39 -0500

    setup script and configuration for distribution

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import re
+
+REQUIREMENTS = ['requests']
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+def find_version(filename):
+    """Reads the version number from a file."""
+    version = ''
+    with open(filename, 'r') as f:
+        regex = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+        for line in fp:
+            match = regex.match(line)
+            if match:
+                version = match.group(1)
+                break
+    if not version:
+        raise RuntimeError('Cannot find version information')
+    return version
+
+__version__ = find_version("textblob/__init__.py")
+
+
+setup(  name='googlesearch',
+        version=__version__,
+        description='Python wrapper around Google Custom Search '
+                    'JSON/Atom API. Supports proxies.',
+        long_description=readme(),
+        license='MIT',
+        author='Irmak Sirer',
+        author_email='irmak.sirer@gmail.com',
+        url='https://github.com/frrmack/googlesearch',
+        packages=find_packages(exclude=('examples', )),
+        zip_safe=False,
+        classifiers=[ 'Development Status :: 4 - Beta',
+                      'Intended Audience :: Developers',
+                      'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
+                      'License :: OSI Approved :: MIT License',
+                      'Programming Language :: Python :: 2',
+                      'Programming Language :: Python :: 2.6',
+                      'Programming Language :: Python :: 2.7',
+                  ],
+        install_requires=REQUIREMENTS,
+        keywords=["googlesearch", "google", 'search', 'web search']
+    )
+
